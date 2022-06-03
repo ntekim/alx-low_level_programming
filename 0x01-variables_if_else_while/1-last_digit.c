acc6245a811952272ed33020d25d62842c601f80
ntekim <jothamntekim@gmail.com>
2022-06-03 11:36:03 -0700

    moved l_digit var decleration to top of main() block

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-	int n;
+	int n, l_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int l_digit = n % 10;
+	l_digit = n % 10;
 	if (l_digit > 5)
 	{
 		printf("The last digit of %d is %d and is greater than 5\n", n, l_digit);
