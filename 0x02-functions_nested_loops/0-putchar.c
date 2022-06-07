f8b32b5021608a6cd4ad78a0d7dcbbdc895b3ab2
ntekim <jothamntekim@gmail.com>
2022-06-07 12:26:37 -0700

    Task 0 - Functions and nested looops
    Print _putchar
    created a header file with _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "0-putchar.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - calls _putchar() from header file
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+//	for (i = 0; i <= strlen(c); i++)
+		_putchar("_putchar");
+
+	_putchar("\n");
+	//printf("%lu", strlen(c));
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _putchar - write data to stdout
+ *
+ * Return: data written to stdin
+ */
+
+int _putchar(char c[20])
+{
+	return (printf("%s", c));
+}
