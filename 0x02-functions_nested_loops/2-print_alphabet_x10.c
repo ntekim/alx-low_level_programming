5bfdc9cd8ec98ea78dd018253cdf6da6e231aaca
Ntekim-Jotham <jothamntekim@gmail.com>
2022-06-08 23:19:37 +0100

    Task 2 - Print Alphabet 10 times
    Added print_alphaet_x10 function protype to main.h header file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lowercased alphabets 10 times
+ *
+ * Return: 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	char l;
+	int count = 0;
+
+	while (count++ <= 9;)
+	{
+		for (l = 'a'; l <= 'z'; l++)
+			_putchar(l);
+
+		_putchar('\n');
+}
