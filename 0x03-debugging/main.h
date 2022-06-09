2f5ff0a55a77b94f2f18dbd32478e57bdb6a5086
ntekim <jothamntekim@gmail.com>
2022-06-09 15:38:41 -0700

    tasks update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
-#ifndef MAIN_H	
+#ifndef MAIN_H
 #define MAIN_H
-/**
- * main header file 
- * define all function prototype for this module
- */
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-#endif
+
+#endif /* MAIN_H /*
