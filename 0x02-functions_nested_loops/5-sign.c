c8975824cdd31db9bde4aada4a81d96d61bdaaea
Ntekim-Jotham <jothamntekim@gmail.com>
2022-06-09 01:13:56 +0100

    Task 6-8 solution

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number ( + positive && - negative)
+ *
+ * @n: character to check
+ *
+ * Return: 1 (success)
+ * O (failure)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar("+");
+	
+	if (n == 0)
+		_putchar("0");
+
+	if (n < 0)
+		_putchar("-);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - prints absolute value of a number
+ * 
+ * @n: character to print
+ *
+ * Return: 0 (success)
+*/
+
+int _abs(int n)
+{
+	int abs = abs(n);
+	_putchar(abs);
+	return (0);
+}
