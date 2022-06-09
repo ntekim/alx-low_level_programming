9ebf7cc2363252fa95c6d893b612971a5636a821
ntekim <jothamntekim@gmail.com>
2022-06-09 12:39:54 -0700

    Task 1 - isdigit function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  isdigit - function
+ *
+ *  @c: parameter to be checked
+ *
+ *  Return: 1 (success)
+ *
+ *  Return: 0 (failure)
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
