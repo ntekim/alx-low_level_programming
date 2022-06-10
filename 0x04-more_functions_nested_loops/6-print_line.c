1c9974ae535e8eaafcda134f5f7c468ecf59bbfc
ntekim <jothamntekim@gmail.com>
2022-06-09 22:48:22 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_line - print _ based on the n value
  *
@@ -11,11 +12,11 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (c = 0; c < n; c++)
-			_putchar("_");
+		for (c = 0; c <= n; c++)
+			putchar('_');
 
-		_putchar("\n");
+		putchar('\n');
 	}
 
-	_putchar("\n");
+	putchar('\n');
 }
