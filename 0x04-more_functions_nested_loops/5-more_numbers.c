1c4ff6ba69cf53190aebd588f89d694a7ca41b89
ntekim <jothamntekim@gmail.com>
2022-06-10 09:40:26 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,15 @@ void more_numbers(void)
 {
 	int n, t;
 
-	for (t = 0; t <= 10; t++)
+	for (t = 0; t < 10; t++)
 	{
 		for (n = 0; n <= 14; n++)
-			putchar((n % 10) + '0');
+		{
+			if (n > 9)
+				_putchar((n / 10) + '0');
 
-		puchar('\n');
-
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
