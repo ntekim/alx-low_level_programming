f1201e8e2f8f5fd67670bca71973d8761e9a6a00
ntekim <jothamntekim@gmail.com>
2022-06-09 22:40:46 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 {
 	int n, t;
 
-	for (t = 0; t < 10; t++)
+	for (t = 0; t <= 10; t++)
 	{
-		for (n = 0; n < 14; n++)
+		for (n = 0; n <= 14; n++)
 			putchar((n % 10) + '0');
 
 		puchar('\n');
