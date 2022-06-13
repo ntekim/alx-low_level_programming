acb6b8ff1629b6b8d274a71679dbd557bcda90a1
ntekim <jothamntekim@gmail.com>
2022-06-13 10:39:47 -0700

    Task 3 - prints a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  _puts - prints a string followed by a new line
+ *  @s : pointer variable of the string to be printed out
+ */
+
+void _puts(char *s)
+{
+	putchar("%s/n", *s);
+}
