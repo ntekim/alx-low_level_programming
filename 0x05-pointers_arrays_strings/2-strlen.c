1ed8b0180e30a976a3fa6242d84dce52ef15df81
ntekim <jothamntekim@gmail.com>
2022-06-13 10:23:16 -0700

    Task 2 - This report, by its very length, defends itself against the risk of being read
    Added 2-strlen.c file
    Function prints length of string
    Added string.h header file to above file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer variable of type char to be worked with
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	len = strlen(*s);
+	return (len);
+}
