4cf63f33b913c4cf9f858700cdc8fc93eebd4fea
ntekim <jothamntekim@gmail.com>
2022-06-13 12:25:37 -0700

    Task 8 - prints specified number of elements in an array based on user input

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,11 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i <= n)
+	while (i < n)
 	{
 		printf("%d", a[i]);
 
-		if (i == n)
+		if (i == (n - 1))
 			continue;
 
 		printf(", ");
