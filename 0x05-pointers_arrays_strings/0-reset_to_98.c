b15880437ccbd2b824420ce6a3034a69fb8e2d41
ntekim <jothamntekim@gmail.com>
2022-06-13 09:49:07 -0700

    fixed betty doc style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 /**
  * reset_to_98 - update the value the pointer variable n
  * that points to an interger takes
+ * @n: pointer variable to be updated
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return;
 }
