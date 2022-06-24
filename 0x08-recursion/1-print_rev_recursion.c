4aaee918de348964e24163e1c0629d33147f8fc7
ntekim <jothamntekim@gmail.com>
2022-06-23 18:38:29 -0700

    task 1 - Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - print string in reverse
+ *
+ * @s: pointer variable
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if(*s)
+	{
+		putchar(s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+		return (0);
+}	
