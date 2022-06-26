8cad5e08baf498734f19742319650a67f1d46f73
Jotham Ntekim <jothamntekim@gmail.com>
2022-06-26 19:48:29 +0000

    Solved unused variables

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,12 +11,10 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc > 1)
+	for (i = 0; i < argc; i++)
 	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
+
 	return (0);
 }
