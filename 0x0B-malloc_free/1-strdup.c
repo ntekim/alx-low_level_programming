3447af03129027781b0f388e8b9e3bb361613000
Jotham Ntekim <jothamntekim@gmail.com>
2022-06-28 13:02:13 +0000

    Task 1 - THe woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,16 +11,23 @@
 char *_strdup(char *str)
 {
 	char *p;
+	int i, len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	p = (char *) malloc(sizeof(char *));
+	for (i = 0; str[i]; i++)
+		len++;
+
+	p = (char *) malloc(sizeof(char *) * (len + 1));
 
 	if (p == NULL)
 		return (NULL);
 
-	p = str;
+	for (i = 0; str[i]; i++)
+		p[i] = str[i];
+
+	p[len] = '\0';
 
 	return (p);
 }
