f2cdc2d5920632ab78122a9a4407bcb4c52064b9
Jotham Ntekim <jothamntekim@gmail.com>
2022-07-05 14:23:30 +0000

    Strucrues and typdef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,10 +9,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog p;
-
-	p = &d;
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
