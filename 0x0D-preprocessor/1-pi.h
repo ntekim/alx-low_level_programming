483eedcec0a7bd4e32e8daa2a5bcf2e06d87dfeb
Jotham Ntekim <jothamntekim@gmail.com>
2022-07-05 13:55:32 +0000

    Task 0x0C - Preprocessor

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,9 @@
 #ifndef PI_H
 #define PI_H
 
-/**
- * DESC: Header file that defines a macro named PI
- *	as an abbreviation for the token 3.14159265359
+/*
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
  */
 
 #define PI 3.14159265359
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-/**
- * DESC:  Header file that defines a function-like macro ABS(x)
+/*
+ * Desc:  Header file that defines a function-like macro ABS(x)
  *	that computes the absolute value of a number x.
  * @x: function parameter
  */
