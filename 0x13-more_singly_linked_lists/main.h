fcb09a1543951a53b72771b72037601501bf3b70
Jotham Ntekim <jothamntekim@gmail.com>
2022-07-17 02:42:12 +0000

    fixed necessary error

--- 0x13-more_singly_linked_lists/main.h
+++ 0x13-more_singly_linked_lists/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
+
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * @digit: integer
  * @next: points to the next node
  *
  * Description: singly linked list for
@@ -17,7 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-size_t listint_t_len(const listint_t *h);
+size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
