9cd5be1caea32cbd1942f0964782a87c0e197e0a
Jotham Ntekim <jothamntekim@gmail.com>
2022-07-19 13:12:22 +0000

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,9 @@
 #include "lists.h"
-/**
- * free_listint - frees a listint-t list
- * @head: linked list to free
- *
- */
 
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
