ca56f27faca6b7faeeb08380df29bb8f8179ffd7
Jotham Ntekim <jothamntekim@gmail.com>
2022-07-26 13:07:12 +0000

    Task 1 updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (access(filename, F_OK) == 0)
 	{
-		o = open(filename, O_TRUNC| O_RDWR);
+		o = open(filename, O_TRUNC | O_RDWR);
 		w = write(o, text_content, len);
 
 		if (o == -1 || w == -1)
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,3 +10,4 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+
