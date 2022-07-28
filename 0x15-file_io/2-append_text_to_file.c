d270b80208c7f69144fcc4be200fc8b337491170
Jotham Ntekim <jothamntekim@gmail.com>
2022-07-28 11:57:14 +0000

    Task - append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,4 +10,33 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int o, w, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		if (access(filename, F_OK) == -1)
+		{
+			if (access(filename, W_OK) == -1)
+				return (-2);
+
+			return (1);
+		}
+		return (-3);
+	}
+
+	for (len = 0; text_content[len];)
+		len++;
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-4);
+
+	close(o);
+
+	return (1);
+}
