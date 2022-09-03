578982788f6a01b4d89e76eeec6f8898d33c5da5
Jotham Ntekim <jothamntekim@gmail.com>
2022-09-03 15:23:49 +0000

    Updated task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - program return node of a doubly list at index 
+ * get_dnodeint_at_index - program return node of a doubly list at index
  * @head: node to work with
  * @index: index of the node
  *
@@ -9,38 +9,16 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *current;
 	unsigned int len = 0;
 
 	if (!head)
 		return (NULL);
 
-	if (index == 0)
+	while (head->next && len < index)
 	{
-		current = malloc(sizeof(dlistint_t));
-		if (!current)
-			return (NULL);
-
-		current->n = head->n;
-		current->next = NULL;
-		current->prev = NULL;
-
-		return (current);
-	}
-
-	while (head->next)
-	{
-		if (index > len)
-			return (NULL);
-
-		if (len == index)
-			current = head;
-
 		head = head->next;
-
 		len++;
 	}
 
-	return (current);
-				
+	return (head ? head : NULL);
 }
