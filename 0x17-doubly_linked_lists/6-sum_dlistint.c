48b5a3b44895b5690cb56a6b922aa250ae7e851f
Jotham Ntekim <jothamntekim@gmail.com>
2022-09-03 15:03:36 +0000

    Updated task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -10,12 +10,15 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
+	if (!head)
+		return (0);
+
 	while (head->next)
 	{
 		sum += head->n;
 		head = head->next;
 	}
-	
+
 	sum += head->n;
 
 	return (sum);
